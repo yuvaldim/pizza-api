44bb5f4a752615401a2081180f8433d2289c9c19
Yuval Dimnik <yuval.dimnik@gmail.com>
2022-07-10 19:45:14 +0300

    chapter 2 is working

---
+++ api.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Api = require('claudia-api-builder')
+const api = new Api()
+const getPizzas = require('./handlers/get-pizzas')
+const createOrder = require('./handlers/create-order')
+
+api.get('/', () => 'Welcome to Pizza API')
+api.get('/pizzas', () => {
+    return getPizzas()
+})
+
+api.get('/pizzas/{id}', (request) => {
+    return getPizzas(request.pathParams.id)
+}, {
+    error: 404
+})
+
+api.post('/orders', (request) => {
+    return createOrder(request.body)
+}, {
+    success: 201,
+    error: 400
+})
+module.exports = api
